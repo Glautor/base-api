799772d1b371cd3caee0e95b990162f573a1eb69
Glauton Santos <glautoncardoso@gmail.com>
2020-03-26 23:31:52 -0300

    Adding service to handle jwt token

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,12 +1,8 @@
 class Api::V1::AuthenticationController < ApplicationController
-    require 'jwt'
-
     def access_token
-        raise 'user_not_found' unless User.find_by_name(authentication_params[:name])
+        return render json: 'user not found' unless User.find_by_name(authentication_params[:name])
         if current_user
-            payload = { data: current_user.id }
-            token = JWT.encode payload, Rails.application.secrets.SECRET_HMAC, 'HS256'
-            render json: token
+            render json: Jwt::Token.new(user: current_user).encode
         else
             render json: 'Wrong password'
         end
