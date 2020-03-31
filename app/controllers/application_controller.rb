25022c9921d432aa65a31e358a9189c6b2e65fca
Glauton Santos <glautoncardoso@gmail.com>
2020-03-31 15:29:53 -0300

    Refactoring code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@ class ApplicationController < ActionController::API
     before_action :verify_access_token, except: :access_token
 
     def verify_access_token
-        hmac_secret = 'my$ecretK3y'
-        decoded_token = JWT.decode request[:header][:authorization], hmac_secret, true, { algorithm: 'HS256' }
-        return true if User.find(decoded_token.data).present?
-        false
+        current_user.present? ? true : false
+    end
+
+    def current_user
+        User.find(decoded_token.data)
+    end
+
+    private
+
+    def decoded_token
+        JWT.decode request[:header][:authorization], Rails.application.secrets.SECRET_HMAC, true, { algorithm: 'HS256' }
     end
 end
