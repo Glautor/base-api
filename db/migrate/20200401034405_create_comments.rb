4877b50a73690284d041dd447e1e632d3ea27e2b
Glauton Santos <glautoncardoso@gmail.com>
2020-04-04 16:02:01 -0300

    Generating model for Comments

--- db/migrate/20200401034405_create_comments.rb
+++ db/migrate/20200401034405_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
       t.string :content
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200327022717) do
+ActiveRecord::Schema.define(version: 20200401034405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema.define(version: 20200327022717) do
     t.string   "confirmation_password"
   end
 
+  add_foreign_key "comments", "users"
 end
