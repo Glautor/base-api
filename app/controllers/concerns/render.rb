9f6f446e15a804c984b489551068e915908b4f43
Glauton Santos <glautoncardoso@gmail.com>
2020-04-04 17:29:56 -0300

    Adding render concern to handle renders on the system

---
+++ app/controllers/concerns/render.rb
@@ -0,0 +1,34 @@
+module Render
+    extend ActiveSupport::Concern
+
+    def json(data, type = :read)
+       return render_without_serializer(data) if Utils::Commons.new.primitive_element?(data)
+       render_serializer(data, type)
+    end
+
+    def json_error(status, message)
+        render json: { message: message }, status: status
+    end
+
+    private
+
+    def render_without_serializer(data)
+        render json: { data: data }, message: :success, status: 200
+    end
+
+    def render_serializer(data, type)
+        render json: serialized_class(data), action: type, message: :success, status: 200
+    end
+
+    def serialized_class(data)
+        Object.const_get("#{class_name(data)}Serializer").new(data).serialized_json
+        rescue NameError
+        raise Utils::Errors::Serializer::ClassNotFound, 'returned data must belong to a serializer class'
+    end
+
+    def class_name(data)
+        data.first.class.name
+        rescue NoMethodError
+        data.class.name
+    end
+end
