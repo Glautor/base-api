7050aa7aa32b4cb324e9bdc5e9da0ad5b689f0c5
Glauton Santos <glautoncardoso@gmail.com>
2020-04-04 17:35:59 -0300

    Adding comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,5 +1,28 @@
 class Api::V1::CommentsController < ApplicationController
+    before_action :set_comment, only: %w[update delete]
     def index
-        render json: { data: Comment.all, message: :success }, status:200
+        json(current_user.comments)
+    end
+
+    def create
+        create(Comment.new, comment_params)
+    end
+
+    def update
+        update(@comment, comment_params)
+    end
+
+    def delete
+        delete(@comment)
+    end
+
+    private
+
+    def set_comment
+        @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+        params.require(:comment).permit(:content, :user_id)
     end
 end
