9702c42fd9f203e9f527fdb7ec0df7214e5f1fb6
Glauton Santos <glautoncardoso@gmail.com>
2020-04-04 17:27:54 -0300

    Adding exception handler concern

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,23 @@
+module ExceptionHandler
+    extend ActiveSupport::Concern
+    
+    included do
+        rescue_from Utils::Errors::User::Unauthorized, with: :handle_unauthorized
+        rescue_from ActionController::BadRequest, with: :handle_bad_request
+        rescue_from Utils::Errors::Serializer::ClassNotFound, with: :handle_internal_server_error
+    end
+
+    private
+
+    def handle_unauthorized
+        json_error(400, :unauthorized)
+    end
+
+    def handle_bad_request
+        json_error(400, :bad_request)
+    end
+
+    def handle_internal_server_error
+        json_error(500, :internal_server_error)
+    end
+end
