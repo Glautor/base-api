267103f1268775ce6a80ff88e539a42c51953b97
Glauton Santos <glautoncardoso@gmail.com>
2020-04-05 00:38:41 -0300

    Modifying encode method to be more general

--- app/services/jwt/token.rb
+++ app/services/jwt/token.rb
@@ -1,7 +1,7 @@
 module Jwt
     class Token
-        def encode(user:)
-            payload = { data: user.id }
+        def encode(data)
+            payload = { data: data }
             JWT.encode payload, Rails.application.secrets.SECRET_HMAC, 'HS256'
         end
 
