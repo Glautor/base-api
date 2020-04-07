6117811cdd548ab9bab50ba973df8878f3c70b3b
Glauton Santos <glautoncardoso@gmail.com>
2020-04-06 23:02:34 -0300

    Adding email field to User Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_034405) do
+ActiveRecord::Schema.define(version: 2020_04_05_040800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_04_01_034405) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "confirmation_password"
+    t.string "email"
   end
 
   add_foreign_key "comments", "users"
