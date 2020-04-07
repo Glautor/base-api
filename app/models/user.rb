341432de837e920a01566f5c1a769fb9de17396b
Glauton Santos <glautoncardoso@gmail.com>
2020-04-06 23:05:28 -0300

    Adding validation for the email presence and uniqueness and removing the uniqueness property from name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
     has_secure_password
     validates_confirmation_of :password
     has_many :comments
