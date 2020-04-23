222ed08c50b1110d183b571cc4fd2496297f6541
Glauton Santos <glautoncardoso@gmail.com>
2020-04-23 13:46:45 -0300

    Adding presence validation for 'content' on Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :user
+  validates_presence_of :content
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'Glauton', email: 'glautorgcs@gmail.com', password: 'test123', password_confirmation: 'test123') }
+
+  context 'is valid if' do
+    it 'the content is present' do
+      expect(Comment.new(content: 'This is my comment', user: user)).to be_valid
+    end
+  end
+
+  context 'is not valid if' do
+    it 'the content is not present' do
+      expect(Comment.new(user: user)).to_not be_valid
+    end
+
+    it 'the user is not present' do
+      expect(Comment.new(content: 'thi is my new comment')).to_not be_valid
+    end
+  end
 end
