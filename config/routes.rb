ab98250536dc487a6db2c5f80415c257a01a8ddd
Glauton Santos <glautoncardoso@gmail.com>
2020-04-30 16:24:39 -0300

    Configuring swagger gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
   namespace :api do
     namespace :v1 do
       namespace :authentication do
